da51933eedb0f94e1fabcdc60ae9406319b7d1e4
noVVait-MAC <jdanov.vad1m@gmail.com>
2020-04-18 14:13:49 +0500

    Fix installing req

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import setuptools
-import pkg_resources
+import requirements
 
-install_reqs = pkg_resources.parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
+install_requires = []
+
+with open('requirements.txt') as fd:
+    for req in requirements.parse(fd):
+        if req.name:
+            name = req.name.replace("-", "_")
+            full_line = name + "".join(["".join(list(spec)) for spec in req.specs])
+            install_requires.append(full_line)
 
 setuptools.setup(
     name="DBBridge",
-    version="0.1dev",
+    version="0.1.dev0",
     packages=setuptools.find_packages(exclude=['tests']),
-    install_requires=reqs,
+    install_requires=install_requires,
     author="Vadim VZ Zhdanov",
     author_email="vz.vadia@gmail.com",
     description="Package for database access",
